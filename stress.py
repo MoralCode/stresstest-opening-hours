e68ac32a6f92abcd8d4d14b8b4c913039a4a7b0f
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-05-06 14:21:53 -0700

    initial stress test script

---
+++ stress.py
@@ -0,0 +1,20 @@
+
+from parse_opening_hours import JsonOpeningHours
+
+
+with open("big list.txt") as fp:
+	success = 0
+	fail = 0
+	for line in fp:
+		if not line:
+			break
+
+		try:
+
+			JsonOpeningHours.parse(line)
+			success += 1
+		except:
+			print("Could not parse: \"{}\"".format(line.strip()))
+			fail += 1
+	
+	print("{} lines total, {} successfully parsed, {} failed. {}% succeeded".format(success+fail, success, fail, success/(success+fail)))
