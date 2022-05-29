add9e2e5f0c79e4f08b10deb7e4cbcf1b55d4118
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-28 20:46:21 -0400

    escape unicode prior to parsing

--- stress.py
+++ stress.py
@@ -9,13 +9,17 @@ with open(DATAFILE, "rb") as fp:
 	for line in fp:
 		if not line:
 			break
-
+		# read as bytes and decode with unicode_escape to catch the escapes
+		line = line.decode("unicode_escape")
+	
+		value = line.strip()#.encode('utf-8', 'ignore')
+	
 		try:
 
 			OpeningHours.parse(line).json()
 			success += 1
-		except:
-			print("Could not parse: \"{}\"".format(line.strip()))
+		except Exception as e:
+			print("Could not parse: \"{}\"".format(value))
 			fail += 1
 	
 	print("Results from {} on {}: {} lines total, {} successfully parsed, {} failed. {:.2f}% succeeded".format(DATAFILE, date.today().isoformat(), success+fail, success, fail, (success/(success+fail))*100))
