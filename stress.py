21e900da64df6810f180f111ef791df88ed391a6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-28 20:45:04 -0400

    include date in output log

--- stress.py
+++ stress.py
@@ -1,6 +1,6 @@
 
 from opening_hours import OpeningHours
-
+from datetime import date
 
 DATAFILE = "data/giscorps.txt"
 with open(DATAFILE, "rb") as fp:
@@ -18,4 +18,4 @@ with open(DATAFILE, "rb") as fp:
 			print("Could not parse: \"{}\"".format(line.strip()))
 			fail += 1
 	
-	print("Results from {}: {} lines total, {} successfully parsed, {} failed. {}% succeeded".format(DATAFILE, success+fail, success, fail, (success/(success+fail))*100))
+	print("Results from {} on {}: {} lines total, {} successfully parsed, {} failed. {}% succeeded".format(DATAFILE, date.today().isoformat(), success+fail, success, fail, (success/(success+fail))*100))
